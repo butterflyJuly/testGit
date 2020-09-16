cfc8a67e1325e14bb9673e127083b5e494477a8c
butterflyJuly <228311683@qq.com>
2020-09-16 21:40:29 +0800

    通过GitHub添加项目

---
+++ src/main/java/com/yu/git/HelloGitHub.java
@@ -0,0 +1,13 @@
+package com.yu.git;
+/**
+* @author yu
+* @date 2020-09-16 21:39:19
+* 类说明
+*/
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("通过GitHub添加项目");
+	}
+
+}
